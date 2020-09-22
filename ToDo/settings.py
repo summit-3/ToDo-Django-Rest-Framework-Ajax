a72bb4a01c708834dac2306535ad9c0841e13860
summit-3 <70266090+summit-3@users.noreply.github.com>
2020-09-22 11:46:50 +0530

    Update settings.py

--- ToDo/settings.py
+++ ToDo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@$j7np_g%zjz=5w!&vli3#x=$2340*cso2#2t@uqnh#pmv3#uv'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
